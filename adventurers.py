d7af60a1ff3aa9a90f1a4e576ce23034595ac49b
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2024-11-07 19:43:32 -0800

    changed adventurer names to dev names; added sean the bard

--- adventurers.py
+++ adventurers.py
@@ -3,29 +3,30 @@ from entities import Entity
 
 
 class Adventurer(Entity):
-    def __init__(self, the_name, the_position, the_hp,
+    def __init__(self, the_name, the_position, the_max_hp,
                  the_attack_speed, the_chance_to_hit, the_damage_range,
                  the_chance_to_block):
-        super().__init__(the_name, the_position, the_hp,
+        super().__init__(the_name, the_position, the_max_hp,
                          the_attack_speed, the_chance_to_hit, the_damage_range)
 
         # adventurer specific attributes (from database)
         self.__my_chance_to_block = the_chance_to_block
 
-    def _update_hp(self, the_diff):
+    def _hit_response(self, the_dmg):
         message = ""
 
         # check block
-        if self.block():
-            message += f"\n{self.get_name()} blocked the attack."
+        if self._block():
+            message += self._block_msg()
         else:
-            super()._update_hp(the_diff)
-            if not self.is_alive():
-                message += f"\n{self.get_name()} fainted."
+            self._update_hp(the_dmg)
 
         return message
 
-    def block(self):
+    def _block_msg(self):
+        return f"\n{self.get_name()} blocked the attack."
+
+    def _block(self):
         blocked = False
         # chance to block (random float within block chance)
         if random.uniform(0,1) <= self.__my_chance_to_block:
@@ -36,6 +37,9 @@ class Adventurer(Entity):
     def get_block_chance(self):
         return self.__my_chance_to_block
 
+    def set_block_chance(self, the_chance_to_block):
+        if 1 >= the_chance_to_block >= 0:
+            self.__my_chance_to_block = the_chance_to_block
 
     def move(self, the_new_position):
         self.set_pos(the_new_position)
@@ -44,10 +48,11 @@ class Adventurer(Entity):
 class Warrior(Adventurer):
     pass
 
-
 class Priest(Adventurer):
     pass
 
-
 class Thief(Adventurer):
+    pass
+
+class Bard(Adventurer):
     pass
