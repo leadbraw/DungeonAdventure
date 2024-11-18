8f39148c4a9af944869a568703247e40295b8c73
Sean Giroux <smgiroux@uw.edu>
2024-11-17 20:29:47 -0800

    Item factory rework, removed unique item naming

--- model/factories/item_factory.py
+++ model/factories/item_factory.py
@@ -1,40 +1,12 @@
-import sqlite3
-import json
-import random
-from copy import deepcopy
-from .item import Item
-
+from model.factories.item import Item
 
 class ItemFactory:
-    def __init__(self, db_path='data/dungeon_game.db'):
-        self.db_path = db_path
-        self.item_cache = self.load_items()  # Cache items for quick access
-
-    def load_items(self):
-        """Loads all items from the database into a cache."""
-        item_cache = {}
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-
-        query = "SELECT id, name, description, attributes, temporary, unique FROM items"
-        cursor.execute(query)
-
-        for row in cursor.fetchall():
-            item_id, name, description, attributes_json, temporary, unique = row
-            attributes = json.loads(attributes_json)  # Parse JSON string into dictionary
-            item_cache[name] = Item(item_id, name, description, attributes, bool(temporary), bool(unique))
-
-        conn.close()
-        return item_cache
-
-    def get_item(self, item_name):
-        """Returns a deep copy of an item by name."""
-        item = self.item_cache.get(item_name)
-        return deepcopy(item) if item else None
-
-    def get_random_non_unique_item(self):
-        """Returns a random non-unique item from the cache."""
-        non_unique_items = [item for item in self.item_cache.values() if not item.my_item_unique]
-        if not non_unique_items:
-            return None  # No non-unique items available
-        return deepcopy(random.choice(non_unique_items))
+    @staticmethod
+    def create_item_from_raw(raw_data):
+        """
+        Create an Item instance from raw database data.
+        :param raw_data: Tuple containing item attributes.
+        :return: An Item instance.
+        """
+        item_id, name, description, ability, temporary, one_time_item = raw_data
+        return Item(item_id, name, description, ability, temporary, one_time_item)
