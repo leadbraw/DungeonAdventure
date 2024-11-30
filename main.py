d0e13cf2115be12855dcf86d05a796c2cb15216b
Sean Giroux <smgiroux@uw.edu>
2024-11-30 02:32:28 -0800

    Removed commented out adventurers data.

--- main.py
+++ main.py
@@ -24,14 +24,6 @@ def main():
     # Initialize GameSetup
     game_setup = GameSetup()
 
-    # # Define adventurers data
-    # adventurers_data = [
-    #     (1, "Mark", "Warrior", 100, 10, 0.8, 15, 25, 0.3),
-    #     (2, "Noah", "Priest", 80, 8, 0.6, 10, 20, 0.4),
-    #     (3, "Thief", "Thief", 70, 12, 0.7, 8, 16, 0.2),
-    #     (4, "Bard", "Bard", 75, 9, 0.65, 10, 18, 0.3),
-    # ]
-
     # Show splash screen while running the game setup
     splash_screen = SplashScreen(screen, font_large)
     splash_screen.display("TEAM 5", game_setup.setup)
