5f568009078431aee68465a907e70d77be0becf2
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2024-12-04 20:24:15 -0800

    fixed class comment. added parameter validation to constructor and property setters. removed type setter. fixed bug where regen did not update hp.

--- model/entities/monsters.py
+++ model/entities/monsters.py
@@ -22,9 +22,13 @@ class Monster(Entity):
                          the_attack_speed, the_hit_chance, the_damage_range)
 
         # Monster-specific attributes
-        self.__my_type = the_type                  # str
-        self.__my_heal_chance = the_heal_chance    # float
-        self.__my_heal_range = the_heal_range      # tuple
+        self.__my_type = the_type.strip() if the_type else "Invalid type"  # Validate type
+        self.__my_heal_chance = min(max(0, the_heal_chance), 1) # Clamp between 0 and 1
+        self.__my_heal_range = (
+            max(1, the_heal_range[0]),
+            max(1, the_heal_range[0], the_heal_range[1])
+            # if heal range is incomplete defaults to 1
+        ) if the_damage_range and len(the_damage_range) == 2 else (1, 1)
 
     ### INTERNAL METHODS ###
     @final
@@ -42,6 +46,7 @@ class Monster(Entity):
             heal = self._regen()
             if heal > 0:
                 message += self._regen_msg(heal)
+                self._update_hp(-heal)
 
         return message
 
@@ -108,11 +113,3 @@ class Monster(Entity):
         :return: The type of the monster.
         """
         return self.__my_type
-
-    @type.setter
-    def type(self, the_type):
-        """
-        Sets the type of the monster.
-        :param the_type: The new type of the monster.
-        """
-        self.__my_type = the_type
