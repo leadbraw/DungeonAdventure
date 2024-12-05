88299f09a70b8611a34e5afe55e707242f975858
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2024-12-04 20:28:37 -0800

    removed untested fixtures. added tests for regen and hit response behaviors.

--- model/tests/test_monsters.py
+++ model/tests/test_monsters.py
@@ -8,20 +8,50 @@ def monster():
     return Monster("Goblin", "Normal", 50, 5, 0.75, (5, 10), 0.5, (5, 15))
 
 @pytest.fixture
-def ogre():
-    return Monster("Ogre", "Normal", 200, 2, 0.6, (30, 60), 0.1, (30, 60))
-
-@pytest.fixture
-def gremlin():
-    return Monster("Gremlin", "Normal", 70, 5, 0.8, (15, 30), 0.4, (20, 40))
-
-@pytest.fixture
-def skeleton():
-    return Monster("Skeleton", "Normal", 100, 3, 0.8, (30, 50), 0.3, (30, 50))
-
-@pytest.fixture
 def elite():
     return Monster("Tom", "Elite", 250, 8, 0.85, (35, 55), 0.4, (38, 50))
+
+# Test regen behavior
+@pytest.mark.parametrize("force_roll, heal, message", [(0, 5, "healed"),
+                                                       (1, 0, "")])
+def test_regen_behavior(monster, mocker, force_roll, heal, message):
+    mocker.patch('random.uniform', return_value=force_roll) # forcing a regen
+    mocker.patch('random.randint', return_value=heal)  # forcing a regen
+    result = monster._hit_response(10)  # Simulating a damage of 10 points
+
+    assert message in result
+    assert monster.max_hp - monster.hp == 10 - heal
+
+
+# Test regen chance setter
+@pytest.mark.parametrize("chance_in, chance_out", [(1.2, 0.5),
+                                                   (-0.1, 0.5),
+                                                   (0.8, 0.8),
+                                                   (0, 0)])
+def test_regen_chance_setter(monster, chance_in, chance_out):
+    monster.heal_chance = chance_in
+    print(monster.heal_chance)
+    assert monster.heal_chance == chance_out
+
+
+# Test heal range setter
+@pytest.mark.parametrize("range_in, range_out", [((-5, 10), (1, 10)),
+                                                 ((5, -10), (5, 5)),
+                                                 ((5, ), (1, 1)),
+                                                 ((5, 10), (5, 10))])
+def test_heal_range_setter(monster, range_in, range_out):
+    monster.damage_range = range_in
+    assert monster.damage_range == range_out
+
+
+# Test Adventurer's _hit_response method for invalid actions
+def test_hit_response_for_dead_monster(monster):
+    # Simulate a dead adventurer (hp = 0)
+    monster.hp = 0
+    result = monster._hit_response(10)
+
+    # Should not perform any action and return empty response
+    assert result == ""
 
 #Item interactions
 def test_take_item_damage(elite):
@@ -39,7 +69,7 @@ def test_take_item_damage_kill(monster):
     assert "has been defeated!" in message
 
 
-def test_take_item_damage_already_defeated(skeleton):
-    skeleton.hp = 0
-    message = skeleton.take_item_damage(10)
+def test_take_item_damage_already_defeated(monster):
+    monster.hp = 0
+    message = monster.take_item_damage(10)
     assert "is already defeated!" in message
